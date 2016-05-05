42344345a4eb84bdf83bc70c1dfaa0db1250903c
Brian Sutherland <brian.sutherland@gmail.com>
2016-05-05 01:29:50 -0700

    added file parsing for all the interfaces, added some test files to run for debugging purposes

--- FileParser/FileParser/MyParser.cs
+++ FileParser/FileParser/MyParser.cs
@@ -8,9 +8,12 @@ namespace FileParser
 {
     public class MyParser
     {
+        public List<User> myUsers { get; set; }
+
         public MyParser(IParser parser, string fileName)
         {
-            parser.ParseFile(fileName);
+            myUsers = new List<User>();
+            myUsers.AddRange(parser.ParseFile(fileName));
         }
 
         public bool SaveInfoToDb()
@@ -21,37 +24,102 @@ namespace FileParser
     }
 
     public interface IParser
-    {
-        void ParseFile(string fileName);
+    {        
+        List<User> ParseFile(string fileName);
     }
 
     public class PipeParser : IParser
     {
-        public void ParseFile(string fileName)
+        public List<User> ParseFile(string fileName)
         {
             // parse the pipe delineated file
+            List<User> userList = new List<User>();
+            using (System.IO.StreamReader file = new System.IO.StreamReader(fileName))
+            {
+                string line;
+
+                while((line = file.ReadLine()) != null)
+                {
+                    string[] userTokens = line.Split('|');
+
+                    // add it to the local List
+                    userList.Add(new User()
+                    {
+                        LastName = Convert.ToString(userTokens[0]),
+                        FirstName = Convert.ToString(userTokens[1]),
+                        Gender = Convert.ToString(userTokens[2]),
+                        FavoriteColor = Convert.ToString(userTokens[3]),
+                        DOB = Convert.ToDateTime(userTokens[4])
+                    });                    
+                }
+            }
 
             Console.WriteLine(string.Format("PIPE ParseFile called on file {0}", fileName));
+
+            return userList;
         }
     }
 
     public class CSVParser : IParser
     {
-        public void ParseFile(string fileName)
+        public List<User> ParseFile(string fileName)
         {
             // parse the csv
+            List<User> userList = new List<User>();
+            using (System.IO.StreamReader file = new System.IO.StreamReader(fileName))
+            {
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] userTokens = line.Split(',');
+
+                    // add it to the local List
+                    userList.Add(new User()
+                    {
+                        LastName = Convert.ToString(userTokens[0]),
+                        FirstName = Convert.ToString(userTokens[1]),
+                        Gender = Convert.ToString(userTokens[2]),
+                        FavoriteColor = Convert.ToString(userTokens[3]),
+                        DOB = Convert.ToDateTime(userTokens[4])
+                    });
+                }
+            }
 
             Console.WriteLine(string.Format("CSV ParseFile called on file {0}", fileName));
+
+            return userList;
         }
     }
 
     public class SpaceParser : IParser
     {
-        public void ParseFile(string fileName)
+        public List<User> ParseFile(string fileName)
         {
             // parse the space delineated file
+            List<User> userList = new List<User>();
+            using (System.IO.StreamReader file = new System.IO.StreamReader(fileName))
+            {
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    string[] userTokens = line.Split(' ');
+
+                    // add it to the local List
+                    userList.Add(new User()
+                    {
+                        LastName = Convert.ToString(userTokens[0]),
+                        FirstName = Convert.ToString(userTokens[1]),
+                        Gender = Convert.ToString(userTokens[2]),
+                        FavoriteColor = Convert.ToString(userTokens[3]),
+                        DOB = Convert.ToDateTime(userTokens[4])
+                    });
+                }
+            }
 
             Console.WriteLine(string.Format("SPACE ParseFile called on file {0}", fileName));
+            return userList;
         }
     }
 }
--- FileParser/FileParser/Program.cs
+++ FileParser/FileParser/Program.cs
@@ -10,17 +10,41 @@ namespace FileParser
     {
         static void Main(string[] args)
         {
+            if(args.Length < 3)
+            {
+                Console.WriteLine("3 files (Piped, CSV, Space) need to be passed in to be parsed");
+                
+                //Console.ReadKey();
+                // TESTING
+                Console.WriteLine("********************\r\nUsing DEFAULT test files\r\n");
+                args = new string[3];
+                args[0] = "pipe.txt";
+                args[1] = "csv.txt";
+                args[2] = "space.txt";
+                //TESTING
+
+                //return;
+            }
+
             List<User> userList = new List<User>();
 
-            MyParser pPipe = new MyParser(new PipeParser(), "fileNamePipe");
-            pPipe.SaveInfoToDb();
+            MyParser pPipe = new MyParser(new PipeParser(), args[0]);
+            userList.AddRange(pPipe.myUsers);
 
-            MyParser pCSV = new MyParser(new CSVParser(), "fileNameCSV");
-            pCSV.SaveInfoToDb();
+            MyParser pCSV = new MyParser(new CSVParser(), args[1]);
+            userList.AddRange(pCSV.myUsers);
 
-            MyParser pSpace = new MyParser(new SpaceParser(), "fileNameSPACE");
-            pSpace.SaveInfoToDb();
+            MyParser pSpace = new MyParser(new SpaceParser(), args[2]);
+            userList.AddRange(pSpace.myUsers);
 
+            Console.WriteLine("Number of users found: {0}", userList.Count);
+            foreach(User u in userList)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4}", u.LastName, u.FirstName, u.Gender, u.FavoriteColor, u.getFormattedDob());
+            }
+
+
+            Console.WriteLine("Press enter to exit...");
             Console.ReadLine();
         }
     }
